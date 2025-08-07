8e7119b0dfc24f8f52bf9f4d3c0be9096004aa82
StanislavKlyuev <patriek@rambler.ru>
2025-08-07 15:31:45 +0300

    Changed output to russian

--- HomeWork1/src/Main.java
+++ HomeWork1/src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello World!");
+        System.out.printf("Привет мир!");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
